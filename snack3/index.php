635c811129fe5c87956c5c466839076d92ce3cf5
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-10 18:08:22 +0200

    feat: snack #3

---
+++ snack3/index.php
@@ -0,0 +1,64 @@
+<?php
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack #3</title>
+</head>
+
+<body>
+    <?php foreach ($posts as $date => $list) { ?>
+        <h2><?php echo $date; ?></h2>
+        <ul>
+            <?php foreach ($list as $post) { ?>
+                <li><?php echo $post['title'] . ' di ' . $post['author'] . ': ' . $post['text']; ?></li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+
+</body>
+
+</html>
