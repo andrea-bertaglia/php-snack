e6825be82b88a29928848d74c0949756878cbff0
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-10 18:46:49 +0200

    feat: snack #6

--- snack6/index.php
+++ snack6/index.php
@@ -1,5 +1,28 @@
 <?php
 
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +32,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Snack #6</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-
+    <?php foreach ($db as $role => $list) { ?>
+        <ul class="<?php echo $role; ?>">
+            <h2><?php echo strtoupper($role); ?></h2>
+            <?php foreach ($list as $person) { ?>
+                <li><?php echo $person['name'] . ' ' . $person['lastname'] ?></li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
 </body>
 
 </html>
